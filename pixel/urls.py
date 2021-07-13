e6939de145b674142a10509594f6400cfdc546bc
Aakriti Sharma <aakriti01as@gmail.com>
2021-07-13 18:58:14 +0530

    Refactor: Change pixel url

--- pixel/urls.py
+++ pixel/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.pixel_view, name='image_load'),
+    path('pixel.gif', views.pixel_view, name='image_load'),
 ]
