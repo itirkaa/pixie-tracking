2d9b52dfb99dff3e5f5eca7657a04413374ba051
Aakriti Sharma <aakriti01as@gmail.com>
2021-07-13 15:37:19 +0530

    Fix: update allowed hosts in setting

--- EmailTracker/settings.py
+++ EmailTracker/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
