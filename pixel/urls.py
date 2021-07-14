f43243ffb54832ea68d9d28293aa629b779dd384
Aakriti Sharma <aakriti01as@gmail.com>
2021-07-14 10:14:24 +0530

    Feat: Change url

--- pixel/urls.py
+++ pixel/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('pixel.gif', views.pixel_view, name='image_load'),
+    path('<str:pixel_id>', views.pixel_view, name='image_load'),
 ]
--- pixel/views.py
+++ pixel/views.py
@@ -14,32 +14,29 @@ key = b'OsuSKe1QDZ0kjJv0GLJI6wB2A267xnVpc5-A1bbCbM4='
 fernet = Fernet(key)
 
 def decrypt_id(pixel_id):
-    result = pixel_id.encode('utf-8')
+    result = bytes(pixel_id, encoding='utf-8')
+    print(result)
     message = fernet.decrypt(result).decode('utf-8')
     return json.loads(message)
 
 
-def pixel_view(request):
+def pixel_view(request, pixel_id):
     pixel_data = base64.b64decode("R0lGODlhAQABAIAAAP8AAP8AACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==")
     event_record = {
         'time': int(time.time()),
         'data': {},
         'headers': {},
     }
-
-    pixel_id = copy.deepcopy(request.GET.get("id"))
+    pixel_id = pixel_id.rstrip('.gif')
     event_record['data'] = decrypt_id(pixel_id)
     for header in request.headers:
         event_record['headers'][header[0]] = request.headers.get(header[0])
 
     print("Saving data to database...")
     
-    tracker, created = Email.objects.get_or_create(pixel_id=pixel_id, date=date.today(), subject=event_record['data']['subject'], user_to=event_record['data']['to'], user_cc=event_record['data']['cc'], seen=0)
+    tracker, created = Email.objects.get_or_create(pixel_id=pixel_id, date=date.today(), subject=event_record['data']['subject'], user_to=event_record['data']['to'], user_cc=event_record['data']['cc'])
     tracker.seen = tracker.seen + 1
     tracker.save()
-    # track = Email(pixel_id=pixel_id, date=date.today(), subject=event_record['data']['subject'], user_to=event_record['data']['to'], user_cc=event_record['data']['cc'], seen=1)
-    # track.save()
-    # consume_open.delay(event_record)
     print(event_record)
     
     return HttpResponse(pixel_data, content_type='image/gif')
